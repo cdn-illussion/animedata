a1de3f1c7dd23f7a6738a25d8a6cb178c6e728ee
cdn-illussion <kompromizz4@gmail.com>
2024-11-15 20:17:57 +0200

    Update data.js

--- data.js
+++ data.js
@@ -10,7 +10,7 @@
     "player_url": "https://example.com/player"
   },
   {
-    "id": 100045,
+    "id": 100046,
     "title_ru": "illussion cdn test",
     "title_en": "Title",
     "description": "Описание аниме",
