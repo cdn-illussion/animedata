c110ee91cfd459670abf418e96b53e5cf45e5262
cdn-illussion <kompromizz4@gmail.com>
2024-11-15 16:01:13 +0200

    Update data.js

--- data.js
+++ data.js
@@ -17,14 +17,14 @@
     "data_drop": 2022,
     "anime_genres": ["Экшен", "Комедия"],
     "poster_original": "https://example.com/poster.jpg",
-     "videos": [
-    {
-      "episode": 1,
-      "fhd": null,
-      "hd": "https://cache.libria.fun/videos/media/ts/3982/1/720/dd6eda1a6621ba2ef43f77b402d1f7e4.m3u8",
-      "sd": "https://cache.libria.fun/videos/media/ts/3982/1/480/580cfdc2ecac1a4998c7bf9e73cf796a.m3u8",
-      "alternative_player_url": "/videos/media/ts/3982/1/480/580cfdc2ecac1a4998c7bf9e73cf796a.m3u8"
-    }
+    "videos": [
+      {
+        "episode": 1,
+        "fhd": null,
+        "hd": "https://cache.libria.fun/videos/media/ts/3982/1/720/dd6eda1a6621ba2ef43f77b402d1f7e4.m3u8",
+        "sd": "https://cache.libria.fun/videos/media/ts/3982/1/480/580cfdc2ecac1a4998c7bf9e73cf796a.m3u8",
+        "alternative_player_url": "/videos/media/ts/3982/1/480/580cfdc2ecac1a4998c7bf9e73cf796a.m3u8"
+      }
     ]
   }
 ]
