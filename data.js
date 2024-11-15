d6e0378c21c2366470de48426a54633f69d5799d
cdn-illussion <kompromizz4@gmail.com>
2024-11-15 15:59:19 +0200

    Update data.js

--- data.js
+++ data.js
@@ -10,13 +10,21 @@
     "player_url": "https://example.com/player"
   },
   {
-    "id": 100012,
+    "id": 100013,
     "title_ru": "illussion cdn test",
     "title_en": "Title",
     "description": "Описание аниме",
     "data_drop": 2022,
     "anime_genres": ["Экшен", "Комедия"],
     "poster_original": "https://example.com/poster.jpg",
-    "player_url": "https://example.com/player"
+     "videos": [
+    {
+      "episode": 1,
+      "fhd": null,
+      "hd": "https://cache.libria.fun/videos/media/ts/3982/1/720/dd6eda1a6621ba2ef43f77b402d1f7e4.m3u8",
+      "sd": "https://cache.libria.fun/videos/media/ts/3982/1/480/580cfdc2ecac1a4998c7bf9e73cf796a.m3u8",
+      "alternative_player_url": "/videos/media/ts/3982/1/480/580cfdc2ecac1a4998c7bf9e73cf796a.m3u8"
+    }
+    ]
   }
 ]
